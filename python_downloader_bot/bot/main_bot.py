3ddef874b269f34ff8b36dd42d6ce3318c1a30bf
Максим Радзей <max.radzey@yandex.ru>
2024-05-23 00:00:02 +0300

    Добавить деббагер сообщений

--- python_downloader_bot/bot/main_bot.py
+++ python_downloader_bot/bot/main_bot.py
@@ -1,3 +1,5 @@
+import logging
+
 from telebot.async_telebot import AsyncTeleBot
 import telebot
 
@@ -6,6 +8,13 @@ from django.conf import settings
 
 bot = AsyncTeleBot(settings.BOT_TOKEN)
 telebot.logger.setLevel(settings.LOG_LEVEL)
+
+logger = logging.getLogger(__name__)
+
+
+# @bot.chat_member_handler()
+# async def chat_member_handler_bot(message):
+#     # logger.info(f'aaaa - {message}')
 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -16,4 +25,14 @@ async def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 async def echo_message(message):
+    user_id = message.from_user.id
+    full_name = message.from_user.full_name
+    username = message.from_user.username
+    text = message.text
+    # logger.info(f'aaaa - {message}')
+    logger.info(f'{user_id=}')
+    logger.info(f'{full_name=}')
+    logger.info(f'{username=}')
+    logger.info(f'{text=}')
+    # print(11)
     await bot.reply_to(message, message.text)
