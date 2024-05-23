f2e080eef5d916f42bdfc598d1bcc3b024cd3782
Максим Радзей <max.radzey@yandex.ru>
2024-05-23 17:15:01 +0300

    Сделать админку, написать модели

--- python_downloader_bot/bot/admin.py
+++ python_downloader_bot/bot/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from django.db.models import ManyToOneRel
 
-# Register your models here.
+from bot.models import TelegramUser
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+def get_fields_from_moddel(db_model):
+    fields = []
+    for field in db_model._meta.get_fields():
+        if isinstance(field, ManyToOneRel):
+            continue
+        fields.append(field.name)
+    return fields
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+
+    list_display = get_fields_from_moddel(TelegramUser)
--- python_downloader_bot/bot/models.py
+++ python_downloader_bot/bot/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    """Users."""
+
+    telegram_id = models.PositiveBigIntegerField(
+        _('telegrma ID'), db_index=True, unique=True
+    )
+    username = models.CharField(
+        _('username'), max_length=150, blank=True, null=True
+    )
+    first_name = models.CharField(
+        _('Имя'), max_length=150, blank=True, null=True
+    )
+    last_name = models.CharField(
+        _('Фамилия'), max_length=150, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
