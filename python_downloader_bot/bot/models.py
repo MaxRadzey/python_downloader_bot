76f373e0475b63019e6d2524fa6162a3fa411546
Максим Радзей <max.radzey@yandex.ru>
2024-05-28 21:09:06 +0300

    Обновить модели без миграций

--- python_downloader_bot/bot/models.py
+++ python_downloader_bot/bot/models.py
@@ -5,6 +5,16 @@ from django.utils.translation import gettext as _
 class TelegramUser(models.Model):
     """Users."""
 
+    USER_ROLE = (
+        ('admin', 'Админ'),
+        ('free_status_user', 'Юзер с ограничениями'),
+        ('paid_status_user', 'Юзер без ограничений')
+    )
+    USER_LANGUAGE = (
+        ('eng', 'Английский'),
+        ('rus', 'Русский')
+    )
+
     telegram_id = models.PositiveBigIntegerField(
         _('telegrma ID'), db_index=True, unique=True
     )
@@ -17,6 +27,15 @@ class TelegramUser(models.Model):
     last_name = models.CharField(
         _('Фамилия'), max_length=150, blank=True, null=True
     )
+    # create_at = models.DateTimeField(auto_now_add=True)
+    # language = models.CharField(
+    #     verbose_name='Язык пользователя', max_length=5,
+    #     choices=USER_LANGUAGE, default='eng'
+    # )
+    # role = models.CharField(
+    #     verbose_name='Статус юзера', max_length=40,
+    #     choices=USER_ROLE, default='free_status_user'
+    # )
 
     class Meta:
         verbose_name = 'Пользователь'
