30ccbeb0492de85e47d800e6d0b2c4517bfa8540
Максим Радзей <max.radzey@yandex.ru>
2024-05-30 18:29:52 +0300

    Добавить словарь с сообщениями на разных языках

---
+++ python_downloader_bot/bot/language_dict.py
@@ -0,0 +1,14 @@
+messages = {
+    'start_message': {
+        'eng': ('Hi, {}, I am TikTok Downloader Bot.\n'
+                'Send me url and i get you a video!'),
+        'rus': ('Привет, {}, я TikTok Downloader Bot.\n'
+                'Отправь мне ссылку и я отправлю тебе видео!')
+    },
+    'help_msg': {
+        'eng': ('Hi, {}, I am TikTok Downloader Bot.\n'
+                'Send me url and i get you a video!'),
+        'rus': ('Привет, {}, я TikTok Downloader Bot.\n'
+                'Отправь мне ссылку и я отправлю тебе видео!')
+    }
+}
