adb7c6693c58ee562c003fa37e644d210fd0947d
Максим Радзей <max.radzey@yandex.ru>
2024-05-30 18:30:52 +0300

    Доабвить миделварь-антифлуд

--- python_downloader_bot/bot/middleware.py
+++ python_downloader_bot/bot/middleware.py
@@ -1,6 +1,7 @@
-from telebot import BaseMiddleware
+from telebot import BaseMiddleware, CancelUpdate
 
 from bot.services import update_or_create_user
+from telebot import types
 
 
 class CustomMiddleware(BaseMiddleware):
@@ -12,7 +13,7 @@ class CustomMiddleware(BaseMiddleware):
         self.update_types = ['message']
 
     async def pre_process_message(self, message, data):
-        # only message update here
+        """Only message update here."""
         my_data = None
         try:
             my_data = getattr(message, 'chat')
@@ -29,13 +30,36 @@ class CustomMiddleware(BaseMiddleware):
         await update_or_create_user(my_data)
 
     async def post_process_message(self, message, data, exception):
-        # only message update here for post_process
+        """Only message update here for post_process."""
         pass
 
     async def pre_process_edited_message(self, message, data):
-        # only edited_message update here
+        """Only edited_message update here."""
         pass
 
     async def post_process_edited_message(self, message, data, exception):
-        # only edited_message update here for post_process
+        """Only edited_message update here for post_process."""
+        pass
+
+
+class AntifloodMiddleware(BaseMiddleware):
+
+    def __init__(self, bot, limit: float) -> None:
+        self.bot = bot
+        self.last_time: dict = {}
+        self.limit: float = limit
+        self.update_types: list = ['message']
+
+    async def pre_process(self, message: types.Message, data):
+        if message.from_user.id not in self.last_time:
+            self.last_time[message.from_user.id] = message.date
+            return
+        if message.date - self.last_time[message.from_user.id] < self.limit:
+            await self.bot.send_message(
+                message.chat.id, 'You are sending messages too often!'
+            )
+            return CancelUpdate()
+        self.last_time[message.from_user.id] = message.date
+
+    async def post_process(self, message, data, exception):
         pass
